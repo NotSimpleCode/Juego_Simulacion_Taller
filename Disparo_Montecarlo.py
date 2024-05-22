5f842844715699de903c0e263f91232aee1da8e4
NotSimpleCode <onejobsdiego@gmail.com>
2024-05-22 14:44:07 -0500

    actualizacion juego

--- Disparo_Montecarlo.py
+++ Disparo_Montecarlo.py
@@ -4,10 +4,10 @@ import Montecarlo as MC
 class DisparoMontecarloClass():
     def __init__(self):
         self.valores_probabilidad_disparo = {
-            60 : 10,  #60 % de probabilidad de hacer 10 de daño - impacto al cuerpo
-            20 : 20,  #20 % de probabilidad de hacer 20 de daño - impacto a piernas
-            15 : 30,  #15 % de probabilidad de hacer 30 de daño - impactos a brazos
-            5 : 60    #5 % de probabilidad de hacer 60 de daño - impacto a cabeza
+            60 : 3,  #60 % de probabilidad de hacer 10 de daño - impacto al cuerpo
+            20 : 5,  #20 % de probabilidad de hacer 20 de daño - impacto a piernas
+            15 : 8,  #15 % de probabilidad de hacer 30 de daño - impactos a brazos
+            5 : 16    #5 % de probabilidad de hacer 60 de daño - impacto a cabeza
         }
         self.mimonte = MC.run(self.valores_probabilidad_disparo, 100000)
     
